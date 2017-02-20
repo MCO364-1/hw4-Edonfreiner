1cb44c420f5a007e69e09cbecc4085ad554f8d8b
edonfreiner <edonfreiner@edons-air.home>
2017-02-19 23:43:54 -0500

    HW4 done

--- src/main/java/mco364/GameOfLife.java
+++ src/main/java/mco364/GameOfLife.java
@@ -14,12 +14,7 @@ import java.util.concurrent.ScheduledThreadPoolExecutor;
 class MyThread extends Thread implements Runnable {
 
     private int row, count;
-    static char[][] nextGen = new char[27][27];
-    
-     MyThread(int row) {
-        this.row = row;
-    }
-
+    static char[][] nextGen;
 
     MyThread(int row, char[][] nextGen) {
         this.row = row;
@@ -33,9 +28,8 @@ class MyThread extends Thread implements Runnable {
     }
 
     private void checkRow() {
-        for (int i = 1; i < GameOfLife.thisGen.length - 1; i++) {
+        for (int i = 1; i < GameOfLife.thisGen.length - 2; i++) {
             count = GameOfLife.neighborCount(row, i);
-
             if (GameOfLife.isAliveNextGeneration(row, i, count)) {
                 nextGen[row][i] = '\u2588';
             }
@@ -47,21 +41,12 @@ public class GameOfLife {
 
     ScheduledThreadPoolExecutor pool;
 
-    GameOfLife() {
-        pool = new ScheduledThreadPoolExecutor(10);
-
-        for (int i = 0; i < 25; i++) {
-            pool.execute(new MyThread(i));
-        }
-
-    }
-
     static char[][] board1 = new char[27][27];
     static char[][] board2 = new char[27][27];
-    static char[][] nextGen;
+    static char[][] nextGen = board1;
+    static char[][] thisGen = board2;
 
-    static char[][] thisGen = new char[25][25];
-    static boolean whichBoard, isTrue;
+    static boolean isThisGen;
     int totalNeighbors;
 
     public static int neighborCount(int row, int col) {
@@ -97,24 +82,22 @@ public class GameOfLife {
     }
 
     public static boolean isAliveNextGeneration(int row, int col, int neighborCount) {
-        thisGen = whichBoard ? board1 : board2;
+        thisGen = isThisGen ? board1 : board2;
 
         if (thisGen[row][col] == '\u2588') {
 
-            if (neighborCount != 2 || neighborCount != 3) {
-                return false;
-            }
-            return true;
-        } else if (neighborCount == 3) {
-            return true;
+            return neighborCount == 2 || neighborCount == 3;
+
+        } else {
+            return neighborCount == 3;
+
         }
 
-        return false;
     }
 
-    public void nextGen() {
-        whichBoard = !whichBoard;
-        nextGen = whichBoard ? board2 : board1;
+    public static void nextGen() {
+        isThisGen = !isThisGen;
+        nextGen = isThisGen ? board2 : board1;
         ScheduledThreadPoolExecutor pool;
         pool = new ScheduledThreadPoolExecutor(10);
 
