08a4e283f64e635f0fd553fb273733aa87406b4f
edonfreiner <edonfreiner@edons-air.home>
2017-02-19 23:43:39 -0500

    HW4 done

--- src/test/java/mco364/GameOfLifeTest.java
+++ src/test/java/mco364/GameOfLifeTest.java
@@ -6,7 +6,17 @@ import static org.junit.Assert.*;
 public class GameOfLifeTest {
 
     @Test
-    public void test1() {
+    public void isAliveNextGenerationTest() {
+        
+        assertTrue(GameOfLife.isAliveNextGeneration(1, 2, 3));
+                
+    }
+
+    @Test
+
+    public void neighborCountTest() {
+        assertEquals(0,GameOfLife.neighborCount(1,2));
+
     }
 
 }
